9b53b4278aad355afc5407baf2982aa17db7cd55
Doozi2 <dooziestreetqk@gmail.com>
2023-07-04 08:02:43 +0000

    copy haptic array

--- mockingly_before.php
+++ mockingly_before.php
@@ -1,1 +1,1 @@
-6Vk0b10o123eebb09b1-972a-4ba2-a7c5-d53a41076b60<".cceCaF*0b0v{{\H2jaO'i9W3fcb7decc-d2ab-440a-a97e-8bc5a169d11c0b1eQ*0]Tk&UtG
+6Vk0b10o123eebb09b1-972a-4ba2-a7c5-d53a41076b60<".cceCaF*0b0v{{\H2jaO'i9W3fcb7decc-d2ab-440a-a97e-8bc5a169d11c0b1eQ*0]Tk&UtGw
